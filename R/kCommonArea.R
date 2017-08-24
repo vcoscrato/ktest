9f6f728f7f2871ef1db7d8bf901f595338fe181b
vcoscrato <30760523+vcoscrato@users.noreply.github.com>
2017-08-23 21:30:56 -0300

    Add files via upload

---
+++ R/kCommonArea.R
@@ -0,0 +1,29 @@
+#' kCommonArea
+#'
+#' Computes the common area between two or more estimed kernel densities.
+#'
+#' @param data A stacked data frame (see ?convertDataStacked).
+#' @param bw The bandwidth used to estimate the kernel densities.
+#' @param npoints The number of points used to estimate the kernel densities.
+#'
+#' @return Common area between the kernel densities.
+#' @export
+#'
+#' @examples data <- convertDataStacked(list(x = rnorm(30), y = rexp(50)))
+#' kCommonArea(data)
+
+kCommonArea <- function(data, bw = bw.nrd0(data[,1]), npoints = 512) {
+  mini <- min(data[,1]) - 3*bw
+  maxi <- max(data[,1]) + 3*bw
+  l <- levels(data[,2])
+  d <- density(data[data[,2] == l[1],1], n = npoints, na.rm = TRUE, from = mini, to = maxi)
+  x <- d[[1]]
+  y <- d[[2]]
+  for(i in 2:length(l)) {
+    d <- density(data[data[,2] == l[i],1], n = npoints, na.rm = TRUE, from = mini, to = maxi)$y
+    for(j in 1:npoints) {
+      y[j] <- min(y[j], d[j])
+    }
+  }
+  return(auc(x, y, type = 'spline'))
+}
